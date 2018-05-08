49910a699a8d74048b1337c65133bf37c33036d9
Jack Nehme <jack.nehme@gmail.com>
2018-05-07 18:51:31 -0700

    Refactored template component

--- quick-start/src/app/app.component.ts
+++ quick-start/src/app/app.component.ts
@@ -1,43 +1,23 @@
 import { Component } from '@angular/core';
+import { Customer, Address } from './model';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-  <h1>{{name}}</h1>
-  <p><i>{{name}} is at {{street}} in {{city}} in the {{region}} region</i></p>
-  <br/>
-
-  <fieldset>
-    <label>Name: <input [(ngModel)]="name"/></label><br/>
-  </fieldset>
-
-  <label><input type="checkbox" [(ngModel)]="hideAddress"/> Hide Address</label>
-
-  <div [hidden]="hideAddress">
-    <fieldset>
-      <label>Street: <input [(ngModel)]="street"/></label>
-    </fieldset>
-    <fieldset>
-      <label>City: <input [(ngModel)]="city"/></label>
-    </fieldset>
-    <fieldset>
-      <label>Region: <select [(ngModel)]="region">
-                      <option>East</option>
-                      <option>North</option>
-                      <option>South</option>
-                      <option>West</option>
-                     </select>
-      </label>
-    </fieldset>
-  </div>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  name = 'Alex Smith';
-  street = '123 Main Street';
-  city = 'Los Angeles';
-  region = 'East';
+  customer: Customer = {
+    id: 1,
+    name: 'Alex Smith',
+    address: {
+      street: '123 Main Street',
+      city: 'Los Angeles',
+      state: 'California',
+      region: 'West',
+    }
+  };
+
   hideAddress = false;
 }
